417dc1a19183ee26101645c1b530ce1e0c991638
KavinduSooriyaarachchi <kavindusooriyaarachchi4@gmail.com>
2024-01-20 22:30:10 +0530

    Finished editprofile page

--- Frontend/Admin/src/pages/EditProfile.js
+++ Frontend/Admin/src/pages/EditProfile.js
@@ -3,6 +3,7 @@ import Sidebar from '../components/Sidebar';
 // import Footer from '../components/Footer';
 import Header from '../components/Header'
 import img1 from '../images/profile.jpg'
+import '../../src/EditProfile.css'
 
 export default function() {
   return (
@@ -24,23 +25,23 @@ export default function() {
           <div id='input-item'>
 
           <label>Name </label> 
-          <input type='text'  required className='placeholder'/>
+          <input type='text'  required className='text-area'/>
           </div>
 
           <div id='input-item'>
-          <label>Email </label> <input type='text'  required className='placeholder'/>
+          <label>Email </label> <input type='text'  required className='text-area'/>
           </div>
           <div id='input-item'>
 
-          <label>Student ID </label> <input type='text'  required className='placeholder'/>
+          <label>Student ID </label> <input type='text'  required className='text-area'/>
           </div>
           <div id='input-item'>
 
-          <label>NIC </label> <input type='text'  required className='placeholder'/>
+          <label>NIC </label> <input type='text'  required className='text-area'/>
           </div>
           <div id='input-item'>
 
-          <label>Contact Number </label> <input type='text'  required className='placeholder'/>
+          <label>Contact Number </label> <input type='text'  required className='text-area'/>
           </div>
           
          
