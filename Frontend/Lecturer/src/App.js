9d800aeb36a7191d63fe6cb1b8e6f84f0e774065
Chamodya Samarasinghe <chamodyasamarasinghe99@gmail.com>
2024-01-30 08:07:07 +0530

    Completed Add_Subject Page

--- Frontend/Lecturer/src/App.js
+++ Frontend/Lecturer/src/App.js
@@ -6,6 +6,8 @@ import Header from './Components/Header/Header';
 import SideMenu from './Components/SideMenu/Sidemenu'
 import Dashboard from './Components/Dashboard/Dashboard';
 import Forget_Password from './Components/Forget_Password/Forget_Password';
+import Add_Subject from './Components/Add_Subject/Add_Subject';
+
 
 function App() {
   return (
@@ -16,9 +18,11 @@ function App() {
         <SideMenu/>
         <Routes>
         <Route exact path='/' element={<Forget_Password/>}/>
+        <Route path='/Forget_Password' element={<Forget_Password/>}/>
         <Route path='/SignUp' element={<SignUp/>}/>
         <Route path='/View_attendance' element={<View_attendance/>}/>
         <Route path='/Dashboard' element={<Dashboard/>}/>
+        <Route path='/Add_Subject' element={<Add_Subject/>}/>
         </Routes>
       </div>
     </Router>
