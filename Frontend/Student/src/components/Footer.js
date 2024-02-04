a7d24703e9a32186fbc3f6aa51a205c244197dbd
shankarr-rm <shankarrr.rm@gmail.com>
2024-02-04 20:38:30 +0530

    Completed the footer part

---
+++ Frontend/Student/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () =>
+    {
+        return(
+            <footer>
+                <p>&copy; 2023 Sabaragamuwa University of SriLanka. All Rights Reserved. </p>
+            </footer>
+        );
+    };
+
+export default Footer;
